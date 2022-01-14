81c3ae1c3d0025975bd4ff45302c0aa645fdf594
rabbithay <thay.csb@gmail.com>
2022-01-14 13:52:09 -0300

    feat: create js mock data

---
+++ src/data/customers.js
@@ -0,0 +1,63 @@
+export const customers = [
+    {
+        id: 1,
+        name: "Samuel Otávio Araújo",
+        cpf: "915.582.030-10",
+        birthdate: "18/12/2007"
+    },
+    {
+        id: 2,
+        name: "Raimundo César Moreira",
+        cpf: "686.595.380-05",
+        birthdate: "11/09/1999"
+    },
+    {
+        id: 3,
+        name: "Vitor Matheus da Luz",
+        cpf: "220.293.440-52",
+        birthdate: "25/08/1980"
+    },
+    {
+        id: 4,
+        name: "Carla Teresinha Vitória Gonçalves",
+        cpf: "542.957.692-15",
+        birthdate: "10/01/1991"
+    },
+    {
+        id: 5,
+        name: "Hugo César Rodrigues",
+        cpf: "108.350.624-22",
+        birthdate: "15/02/2002"
+    },
+    {
+        id: 6,
+        name: "Sophia Sebastiana Manuela Oliveira",
+        cpf: "363.995.868-36",
+        birthdate: "04/07/2003"
+    },
+    {
+        id: 7,
+        name: "Lucas Tiago Anderson Viana",
+        cpf: "069.332.256-07",
+        birthdate: "30/08/1970"
+    },
+    {
+        id: 8,
+        name: "Emanuelly Jéssica",
+        cpf: "633.272.758-66",
+        birthdate: "13/04/1998"
+    },
+    {
+        id: 9,
+        name: "Cauã Rodrigo Luiz Viana",
+        cpf: "199.145.411-27",
+        birthdate: "22/03/1975"
+    },
+    {
+        id: 10,
+        name: "José Silva Olveira",
+        cpf: "779.956.772-43",
+        birthdate: "10/12/2000"
+    },
+    
+]
---
+++ src/data/movies.js
@@ -0,0 +1,54 @@
+export const movies = [
+    {
+        id: 1,
+        title: "O jogo da imitação",
+        parentalRating: "12",
+        release: "0"
+    },{
+        id: 2,
+        title: "A teoria de tudo",
+        parentalRating: "10",
+        release: "0"
+    },{
+        id: 3,
+        title: "Piratas do Caribe",
+        parentalRating: "14",
+        release: "0"
+    },{
+        id: 4,
+        title: "Não olhe para cima",
+        parentalRating: "18",
+        release: "1"
+    },{
+        id: 5,
+        title: "Vingança e Castigo",
+        parentalRating: "18",
+        release: "0"
+    },{
+        id: 6,
+        title: "O Esquadrão Suicida",
+        parentalRating: "16",
+        release: "0"
+    },{
+        id: 7,
+        title: "Eternos",
+        parentalRating: "14",
+        release: "1"
+    },{
+        id: 8,
+        title: "Shang-Chi e a Lenda dos Dez Anéis",
+        parentalRating: "10",
+        release: "1"
+    },{
+        id: 9,
+        title: "Homem-Aranha: Sem Volta para Casa",
+        parentalRating: "12",
+        release: "1"
+    },{
+        id: 10,
+        title: "Uma mente brilhante",
+        parentalRating: "10",
+        release: "0"
+    },
+
+]
